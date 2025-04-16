b668cc1b7b374ceb4b70c84bde13bae18367673b
prashant-singh07 <prashantthane07@gmail.com>
2025-04-17 03:33:50 +0530

    [Get all suppliers]

--- src/queries/supplierQueries.js
+++ src/queries/supplierQueries.js
@@ -6,12 +6,12 @@ const supplierQueries = {
     console.log("shopId", shopId);
     const { data, error } = await supabase
       .from("suppliers")
-      .select("*")
-      // .select("id, name, mobile, address, email_id, gst_number, updated_at")
+      // .select("*")
+      .select("id, name, mobile, address, email_id, gst_number, updated_at")
       .eq("created_id", userId)
       .eq("shop_id", shopId)
-      // .order("updated_at", { ascending: false })
-      .single();
+      .order("updated_at", { ascending: false })
+      .maybeSingle();
 
     if (error) {
       console.error("Error fetching suppliers:", error);
